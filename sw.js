b0d2e23759631b1b689c6141e890645410a271a1
kkyareuk <kkyaareuk@gmail.com>
2026-07-29 19:22:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lifelog-v9-api-satellite-korean-20260729';
+const CACHE='lifelog-v11-google-map-relations-20260729';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-32.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
