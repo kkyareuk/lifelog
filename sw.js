3c5b86a22308c137fa40471e7998d81df56e82e8
kkyareuk <kkyaareuk@gmail.com>
2026-07-29 23:34:16 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lifelog-v27-home-cohabit-20260729';
+const CACHE='lifelog-v28-city-floorplans-20260729';
 const ASSETS=['./','./index.html','./login.html','./payment.html','./payment-success.html','./payment-fail.html','./social-preview.png','./manifest.webmanifest','./icons/icon-32.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
