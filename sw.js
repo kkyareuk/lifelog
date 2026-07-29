50c5ced75af93d66d5556d1b78463aa4e1f04f53
kkyareuk <kkyaareuk@gmail.com>
2026-07-29 20:04:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lifelog-v15-advanced-character-markers-20260729';
+const CACHE='lifelog-v16-projection-markers-live-theme-20260729';
 const ASSETS=['./','./index.html','./config.js','./manifest.webmanifest','./icons/icon-32.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
