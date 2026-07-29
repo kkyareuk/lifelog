062ccec1580da0e5aa7345cd1658238f02f004a3
kkyareuk <kkyaareuk@gmail.com>
2026-07-29 21:44:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
-const CACHE='lifelog-v21-owner-config-wizard-20260729';
-const ASSETS=['./','./index.html','./login.html','./payment.html','./payment-success.html','./payment-fail.html','./config.js','./manifest.webmanifest','./icons/icon-32.png','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='lifelog-v22-config-login-fix-20260729';
+const ASSETS=['./','./index.html','./login.html','./payment.html','./payment-success.html','./payment-fail.html','./manifest.webmanifest','./icons/icon-32.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ const url=new URL(e.request.url);
+ if(url.pathname.endsWith('/config.js')){
+  e.respondWith(fetch(e.request,{cache:'no-store'}));
+  return;
+ }
+ e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
