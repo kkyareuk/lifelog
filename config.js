5717b3ba621b9b2350383b73d146095bf548f3ac
kkyareuk <kkyaareuk@gmail.com>
2026-07-30 21:16:52 +0900

    Update Firebase configuration in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PARALLEL_CITY_FIREBASE = window.PARALLEL_CITY_FIREBASE || {
-  apiKey: "", authDomain: "", projectId: "", storageBucket: "",
-  messagingSenderId: "", appId: ""
+  apiKey: "AIzaSyAC9QuqnZK48KA4IzcsqPxAGvHeaX2IqTs", authDomain: "lifelog-98fff.firebaseapp.com", projectId: "lifelog-98fff", storageBucket: "lifelog-98fff.firebasestorage.app",
+  messagingSenderId: "115165634004", appId: "1:115165634004:web:2cd1e788a55b511643ca41"
 };
