d38cb00ee0ef0fe42b8b1392f18079586e82639a
kkyareuk <kkyaareuk@gmail.com>
2026-08-15 17:37:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION="drawer-village-v20260815ap";
+const CACHE_VERSION="drawer-village-v20260815ar";
 const CORE=[
   "./",
   "./index.html",
--- views.js
+++ views.js
@@ -1,5 +1,5 @@
-import {state,active,characterViewFor,explicitCharacterViewFor} from "./state.js?v=20260811ab";
-import {eventFor as simulateEventFor,visibleTimeline as simulateVisibleTimeline,charactersAtPlace,homeGroups} from "./simulation.js?v=20260811ab";
+import {state,active,characterViewFor,explicitCharacterViewFor} from "./state.js?v=20260815ar";
+import {eventFor as simulateEventFor,visibleTimeline as simulateVisibleTimeline,charactersAtPlace,homeGroups} from "./simulation.js?v=20260815ar";
 // Cache-busted state module is imported above; this comment intentionally keeps the view bundle versioned.
 const esc=(x="")=>String(x).replace(/[&<>"']/g,m=>({"&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#39;"}[m]));
 const I18N={
